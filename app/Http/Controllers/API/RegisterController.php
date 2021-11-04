68da208594722c4c845bae6e3f7efe3cf8410e4a
idadrusdiana <idadrusdiana01@gmail.com>
2021-11-04 13:04:00 +0700

    first commit

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends BaseController
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user =  User::create($input);
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        $success['name'] = $user->name;
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+
+    public function getRegister(Request $request)
+    {
+        // $data = User::all();
+
+        //  return $this->sendResponse();
+    }
+
+    //login API
+
+    public function login(Request $request)
+    {
+        $identifier = filter_var($request->email, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+        if(Auth::attempt([$identifier => $request->email, 'password' => $request->password])) {
+            $user =  User::find(Auth::user()->id);
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+            $success['name'] = $user->name;
+
+            return $this->sendResponse($success, 'User login successfully.');
+        }
+        else{
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        }
+    }
+}
